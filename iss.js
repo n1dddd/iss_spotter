5708aa154ee800e6c8d2145d5f41dc160e279bbb
Daniel Seniv <danielseniv@outlook.com>
2022-12-19 03:29:49 +0000

    Add function to fetch coordinates based on IP

--- iss.js
+++ iss.js
@@ -9,13 +9,39 @@ const fetchMyIP = function(callback) {
     if (response.statusCode !== 200) {
       const msg = `Status code ${response.statusCode} when fetching IP. Response ${body}`;
       callback(Error(msg), null);
-    }
-    else if (!error) {
+    } else if (!error) {
       const data = JSON.parse(body);
       callback(error,data.ip);
-    };
+      userIP = data.ip;
+    }
+  });
+};
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error,null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching IP. Response ${body}`;
+      callback(Error(msg), null);
+    } else if (!error) {
+      const data = JSON.parse(body);
+      if (data.success === false) {
+        const msg = `Error: Success status was ${data.success}. Server message says: ${data.message} when fetching for IP ${ip}`;
+        callback(Error(msg),null);
+        return;
+      } else {
+        const location = {
+          latitude: data.latitude,
+          longitude: data.longitude
+        };
+        callback(error,location);
+      }
+    }
   });
 };
 module.exports = {
-  fetchMyIP
+  fetchMyIP,
+  fetchCoordsByIP
 };
