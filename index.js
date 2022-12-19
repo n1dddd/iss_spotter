7b9312d760d51a395de1e4d01dcecde07b7f634c
Daniel Seniv <danielseniv@outlook.com>
2022-12-19 03:28:27 +0000

    Log latitude/longitude data based on ip

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const {fetchMyIP} = require('./iss');
+const {fetchMyIP, fetchCoordsByIP} = require('./iss');
+
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -7,3 +8,11 @@ const {fetchMyIP} = require('./iss');
 //     console.log("It worked! Returned IP: ", ip);
 //   }
 // });
+fetchCoordsByIP('72.140.98.168', (error,data) => {
+  if (error) {
+    console.log("It didn't work!\n", error.message);
+    return;
+  } else {
+    console.log(data);
+  }
+});
