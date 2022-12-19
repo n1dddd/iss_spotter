ff94ed7d486afc1c96598ce4c0a4137d3f0a3916
Daniel Seniv <danielseniv@outlook.com>
2022-12-19 04:02:34 +0000

    Add function to fetch ISS fly over, using function logic in index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {fetchMyIP, fetchCoordsByIP} = require('./iss');
+const {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTime} = require('./iss');
 
 
 // fetchMyIP((error, ip) => {
@@ -8,11 +8,22 @@ const {fetchMyIP, fetchCoordsByIP} = require('./iss');
 //     console.log("It worked! Returned IP: ", ip);
 //   }
 // });
-fetchCoordsByIP('72.140.98.168', (error,data) => {
-  if (error) {
-    console.log("It didn't work!\n", error.message);
-    return;
-  } else {
-    console.log(data);
-  }
-});
+// fetchCoordsByIP('72.140.98.168', (error,data) => {
+//   if (error) {
+//     console.log("It didn't work!\n", error.message);
+//     return;
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// const coords = {latitude: 45.5016889, longitude: -73.567256}
+
+// fetchISSFlyOverTime(coords, (error,data) => {
+//   if (error) {
+//     console.log("It didn't work!\n", error.message);
+//     return;
+//   } else {
+//     console.log(data);
+//   }
+// })
