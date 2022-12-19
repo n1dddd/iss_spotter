289521a8d8e7c333e8dbaf668d25e1ca85326f55
Daniel Seniv <danielseniv@outlook.com>
2022-12-19 04:03:08 +0000

    Add function to fetch ISS fly over data, pass back results to callback

--- iss.js
+++ iss.js
@@ -40,7 +40,22 @@ const fetchCoordsByIP = function(ip, callback) {
     }
   });
 };
+const fetchISSFlyOverTime = function(coords,callback) {
+  request(`https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`, (error,response,body) => {
+    if (error) {
+      callback(error,null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching IP. Response ${body}`;
+      callback(Error(msg), null);
+    } else if (!error) {
+      console.log(body);
+    }
+  });
+};
 module.exports = {
   fetchMyIP,
-  fetchCoordsByIP
+  fetchCoordsByIP,
+  fetchISSFlyOverTime
 };
